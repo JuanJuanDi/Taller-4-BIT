c4d143857b457504e29945cd0d9e29e470f4e287
Juan Diego <juanndiiego2@gmail.com>
2024-05-11 20:41:40 -0500

    Ejercicio 3

---
+++ JavaScript/t03.js
@@ -0,0 +1,13 @@
+// Escribe un programa que le pida al usuario un número e imprima si es un múltiplo de 5 o no.
+
+
+let numero = prompt("Por favor, ingresa un número:");
+
+numero = parseInt(numero);
+
+if (numero % 5 === 0) {
+    console.log(numero + " es un múltiplo de 5.");
+} else {
+    console.log(numero + " no es un múltiplo de 5.");
+}
+
